df5f987371a7bb55a9e138836d3ece9bf4a6d439
Sanjarbek9706 <san19970618@gmail.com>
2026-04-24 07:34:42 +0900

    A - Task

--- train.js
+++ train.js
@@ -9,15 +9,17 @@ Callback amallarni NodeJS  backend serverda ahamiyati */
 
 // noddejs single thread => thread poollar  kami 4ga bolinadi
 
-console.log("Jack Ma maslahatlari");
-const list = [
-  "yahshi talaba boling", // 0-20
-  "togri boshliq tanlang va koproq hato qiling", // 20-30
-  "uzingizga ishlashingizni boshlang", // 30-40
-  "siz kuchli bolgan narsalarni qiling", // 40-50
-  "yoshlarga investitsiya qiling", // 50-60
-  "endi dam oling, foydasi yoq endi", // 60
-];
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//   "yahshi talaba boling", // 0-20
+//   "togri boshliq tanlang va koproq hato qiling", // 20-30
+//   "uzingizga ishlashingizni boshlang", // 30-40
+//   "siz kuchli bolgan narsalarni qiling", // 40-50
+//   "yoshlarga investitsiya qiling", // 50-60
+//   "endi dam oling, foydasi yoq endi", // 60
+// ];
+
+// Callback
 
 // function maslahatBering(a, callback) {
 //     if(typeof a !== 'number')callback("insert a number", null);
@@ -48,36 +50,37 @@ const list = [
 
   Callback vs Asynchronous vs Promise */
 
-async function maslahatBering(a) {
-  if (typeof a !== "number")
-    throw new Error(
-      "insert a number",
-      null,
-    );
-  else if (a <= 20) return list[0];
-  else if (a > 20 && a <= 30)
-    return list[1];
-  // callback(null, list[1]);
-  else if (a > 30 && a <= 40)
-    return list[2];
-  // callback(null, list[2]);
-  else if (a > 40 && a <= 50)
-    return list[3];
-  // callback(null, list[3]);
-  else if (a > 50 && a <= 60)
-    return list[4];
-  // callback(null, list[4]);
-  else {
-    // return list[5];
-    return new Promise(
-      (resolve, reject) => {
-        setTimeout(function () {
-          return list[5];
-        }, 5000);
-      },
-    );
-  }
-}
+//ASYN function
+// async function maslahatBering(a) {
+//   if (typeof a !== "number")
+//     throw new Error(
+//       "insert a number",
+//       null,
+//     );
+//   else if (a <= 20) return list[0];
+//   else if (a > 20 && a <= 30)
+//     return list[1];
+//   // callback(null, list[1]);
+//   else if (a > 30 && a <= 40)
+//     return list[2];
+//   // callback(null, list[2]);
+//   else if (a > 40 && a <= 50)
+//     return list[3];
+//   // callback(null, list[3]);
+//   else if (a > 50 && a <= 60)
+//     return list[4];
+//   // callback(null, list[4]);
+//   else {
+//     // return list[5];
+//     return new Promise(
+//       (resolve, reject) => {
+//         setTimeout(function () {
+//           return list[5];
+//         }, 5000);
+//       },
+//     );
+//   }
+// }
 //  console.log("passed here 0");
 //  maslahatBering(25).then(data => {
 //    console.log("javob", data);
@@ -87,12 +90,18 @@ async function maslahatBering(a) {
 // console.log("passed here 1");
 
 //asyn/await
-async function run() {
-  let javob = await maslahatBering(20);
-  console.log(javob);
-  javob = await maslahatBering(30);
-  console.log(javob);
-  javob = await maslahatBering(42);
-  console.log(javob);
+// async function run() {
+//   let javob = await maslahatBering(20);
+//   console.log(javob);
+//   javob = await maslahatBering(30);
+//   console.log(javob);
+//   javob = await maslahatBering(42);
+//   console.log(javob);
+// }
+// run();
+
+// A-Task
+function harfsana(harf, matn) {
+  return matn.split(harf).length - 1;
 }
-run();
+console.log(harfsana("a", "sanjarbek"));
