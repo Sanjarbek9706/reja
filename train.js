01de7ac5ec5c7126805d25a2ff15d19435fcde9b
Sanjarbek9706 <san19970618@gmail.com>
2026-04-27 07:06:37 +0900

    B - Task

--- train.js
+++ train.js
@@ -9,37 +9,37 @@ Callback amallarni NodeJS  backend serverda ahamiyati */
 
 // noddejs single thread => thread poollar  kami 4ga bolinadi
 
-console.log("Jack Ma maslahatlari");
-const list = [
-  "yahshi talaba boling", // 0-20
-  "togri boshliq tanlang va koproq hato qiling", // 20-30
-  "uzingizga ishlashingizni boshlang", // 30-40
-  "siz kuchli bolgan narsalarni qiling", // 40-50
-  "yoshlarga investitsiya qiling", // 50-60
-  "endi dam oling, foydasi yoq endi", // 60
-];
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//   "yahshi talaba boling", // 0-20
+//   "togri boshliq tanlang va koproq hato qiling", // 20-30
+//   "uzingizga ishlashingizni boshlang", // 30-40
+//   "siz kuchli bolgan narsalarni qiling", // 40-50
+//   "yoshlarga investitsiya qiling", // 50-60
+//   "endi dam oling, foydasi yoq endi", // 60
+// ];
 
-// Callback
+// // Callback
 
-function maslahatBering(a, callback) {
-  if (typeof a !== "number")
-    callback("insert a number", null);
-  else if (a <= 20)
-    callback(null, list[0]);
-  else if (a > 20 && a <= 30)
-    callback(null, list[1]);
-  else if (a > 30 && a <= 40)
-    callback(null, list[2]);
-  else if (a > 40 && a <= 50)
-    callback(null, list[3]);
-  else if (a > 50 && a <= 60)
-    callback(null, list[4]);
-  else {
-    setTimeout(function () {
-      callback(null, list[5]);
-    }, 5000);
-  }
-}
+// function maslahatBering(a, callback) {
+//   if (typeof a !== "number")
+//     callback("insert a number", null);
+//   else if (a <= 20)
+//     callback(null, list[0]);
+//   else if (a > 20 && a <= 30)
+//     callback(null, list[1]);
+//   else if (a > 30 && a <= 40)
+//     callback(null, list[2]);
+//   else if (a > 40 && a <= 50)
+//     callback(null, list[3]);
+//   else if (a > 50 && a <= 60)
+//     callback(null, list[4]);
+//   else {
+//     setTimeout(function () {
+//       callback(null, list[5]);
+//     }, 5000);
+//   }
+// }
 //  console.log("passed here 0");
 // maslahatBering(30, (err,  data) => {
 //     if(err) console.log('ERROR', err);
@@ -95,38 +95,61 @@ function maslahatBering(a, callback) {
 // });
 // console.log("passed here 1");
 
-function maslahatBering(a, callback) {
-  if (typeof a !== "number")
-    callback("insert a number", null);
-  else if (a <= 20)
-    callback(null, list[0]);
-  else if (a > 20 && a <= 30)
-    return list[1];
-  else if (a > 30 && a <= 40)
-    return list[2];
-  else if (a > 40 && a <= 50)
-    return list[3];
-  else if (a > 50 && a <= 60)
-    return list[4];
-  else {
-    setTimeout(function () {
-      return list[0];
-    }, 5000);
-  }
-}
+// function maslahatBering(a, callback) {
+//   if (typeof a !== "number")
+//     callback("insert a number", null);
+//   else if (a <= 20)
+//     callback(null, list[0]);
+//   else if (a > 20 && a <= 30)
+//     return list[1];
+//   else if (a > 30 && a <= 40)
+//     return list[2];
+//   else if (a > 40 && a <= 50)
+//     return list[3];
+//   else if (a > 50 && a <= 60)
+//     return list[4];
+//   else {
+//     setTimeout(function () {
+//       return list[0];
+//     }, 5000);
+//   }
+// }
 
-await async function run() {
-  let javob = await maslahatBering(20);
-  console.log(javob);
-  javob = await maslahatBering(30);
-  console.log(javob);
-  javob = await maslahatBering(42);
-  console.log(javob);
-};
-run();
+// await async function run() {
+//   let javob = await maslahatBering(20);
+//   console.log(javob);
+//   javob = await maslahatBering(30);
+//   console.log(javob);
+//   javob = await maslahatBering(42);
+//   console.log(javob);
+// };
+// run();
 
 // // A-Task
 // function harfsana(harf, matn) {
 //   return matn.split(harf).length - 1;
 // }
 // console.log(harfsana("a", "sanjarbek"));
+
+// B- Task
+
+function countDigits(string) {
+  let count = 0;
+  for (
+    let a = 0;
+    a < string.length;
+    a++
+  ) {
+    if (
+      !isNaN(string[a]) &&
+      string[a] !== " "
+    ) {
+      count++;
+    }
+  }
+  return count;
+}
+const natija = countDigits(
+  "saoijfijf438743tgfjn",
+);
+console.log("Raqamlar soni:", natija);
