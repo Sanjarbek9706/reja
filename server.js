dc2ede1b29331536958cd768d8ea66c825784c34
Sanjarbek9706 <san19970618@gmail.com>
2026-05-09 20:19:27 +0900

    reja loyiha dizayini yangilandi

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ mongodb.connect(
       const app = require("./app");
       const server =
         http.createServer(app);
-      let PORT = 3001;
+      let PORT = 3009;
       server.listen(PORT, function () {
         console.log(
           `The server is running. succesfully on port: ${PORT}, http://localhost:${PORT}`,
